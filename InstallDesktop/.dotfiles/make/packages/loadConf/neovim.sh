5c57e541e79228109c1e94d67ec20c94bc89ff81
flynth <flynth.dev@gmail.com>
2023-03-15 17:31:33 -0400

    Delegando la instalacion a nvim

--- InstallDesktop/.dotfiles/make/packages/loadConf/neovim.sh
+++ InstallDesktop/.dotfiles/make/packages/loadConf/neovim.sh
@@ -1,8 +1,7 @@
 
 function main
 {
-	# Peacker lua
-	git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
+
 	# crear enlace en .config
 	rm -r $HOME/.config/nvim 
 	ln -s $HOME/.dotfiles/config/nvim $HOME/.config/nvim
