83ec4121349262a50374fb4abaf8f63f42bd5ddc
flynth <flynth.dev@gmail.com>
2023-04-27 12:48:36 -0400

    include option delete
      PaperBin.sh * -d

--- InstallDesktop/.dotfiles/utils/scripts/PaperBin.sh
+++ InstallDesktop/.dotfiles/utils/scripts/PaperBin.sh
@@ -1,7 +1,22 @@
 function main 
 {
 	local path="$HOME/.tmp"
-  mv $* $path
+  local args=($*)
+  #delete 
+  local lenth=${#args[@]}
+  local options=${args[lenth-1]}
+
+  case $options in
+    -d)
+      echo "delete"
+      args=("${args[@]/-d}")
+      rm -rf ${args[@]}
+      ;;
+    *)
+      echo "move to ~/.tmp"
+      mv $* $path
+      ;;
+  esac
 }
 
 main $*
